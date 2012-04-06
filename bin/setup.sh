c2ac47fb9d5d082dc764db26dcc92c875d0632d2
Lex Toumbourou <lextoumbourou@gmail.com>
2012-04-05 21:22:28 -0400

    New setup script and tmux config

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,5 +1,37 @@
 #!/bin/bash
-ln -s ~/dotfiles/vim ~/.vim
-ln -s ~/dotfiles/vim/vimrc ~/.vimrc
-ln -s ~/dotfiles/bash/bashrc ~/.bashrc
-ln -s ~/dotfiles/screen/screenrc ~/.screenrc
+
+function check_or_move {
+	#
+	# If file exists and isn't already symbolic link
+	# move to a backup
+	#
+	check_file=$1
+	backup_file=$1'.backup'
+	cd $HOME
+
+	if [ -L $check_file ]
+	then
+		rm -v $check_file
+	fi
+
+	if [ -f $check_file ] 
+	then
+		mv -v $check_file $backup_file
+	fi
+
+}
+
+check_or_move '.vim'
+ln -sv ~/dotfiles/vim ~/.vim
+
+check_or_move '.vimrc'
+ln -sv ~/dotfiles/vim/vimrc ~/.vimrc
+
+check_or_move '.bashrc'
+ln -sv ~/dotfiles/bash/bashrc ~/.bashrc
+
+check_or_move '.screenrc'
+ln -sv ~/dotfiles/screen/screenrc ~/.screenrc
+
+check_or_move '.tmux.conf'
+ln -sv ~/dotfiles/tmux/tmux.conf ~/.tmux.conf
