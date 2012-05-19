31072603632c1a75d68cb8f4e248a272cfe26355
Lex Toumbourou <lextoumbourou@gmail.com>
2012-05-19 20:14:47 +1000

    More bashrc changes

--- bin/setup.sh
+++ bin/setup.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
 function check_or_move {
-	#
-	# If file exists and isn't already symbolic link
-	# move to a backup
-	#
-	check_file=$1
-	backup_file=$1'.backup'
-	cd $HOME
+    # If file exists and isn't already symbolic link move to a backup
+    check_file=$1
+    backup_file=$1'.backup'
+    cd $HOME
 
-	if [ -L $check_file ]
-	then
-		rm -v $check_file
-	fi
+    if [ -L $check_file ]
+    then
+            rm -v $check_file
+    fi
 
-	if [ -f $check_file ] 
-	then
-		mv -v $check_file $backup_file
-	fi
+    if [ -f $check_file ] 
+    then
+            mv -v $check_file $backup_file
+    fi
 
 }
+
+# Create a host specific bash file
+touch ~/.host.bashrc
 
 check_or_move '.vim'
 ln -sv ~/dotfiles/vim ~/.vim
